4cb683b55671c7827825c9ea60adacf3fb8f034e
hhuang90 <hhuang0402@gmail.com>
2018-07-13 16:23:55 -0400

    edit namespace

--- R/TVD.R
+++ R/TVD.R
@@ -1,6 +1,51 @@
-#' @export TVDMSS
+.findPos=function(target,sc)
+{
+  outcome=NULL;
+  if(length(target)>0)
+  {
+    outcome=integer(length(target));
+    for(i in 1:length(outcome))
+      outcome[i]=which(sc==target[i]);
+  }
+  return(outcome);
+}
+
+.fbplotDetect<-function(data,depth,shapeOutlier)
+{
+  nCurve=dim(data)[2];
+  nCentral=ceiling(nCurve/2);
+
+  remain=1:nCurve;
+  if(!is.null(shapeOutlier))
+  {
+    data=data[,-shapeOutlier]
+    depth=depth[-shapeOutlier]
+    remain=remain[-shapeOutlier];
+  }
+  index=order(depth,decreasing=T);
+
+  center=data[,index[1:nCentral]];
+  inf=apply(center,1,min);
+  sup=apply(center,1,max)
+  dist=1.5*(sup-inf);
+  upper=sup+dist;
+  lower=inf-dist;
+
+  outly=(data<=lower)+(data>=upper);
+  outpoint=which(colSums(outly)>0);
+  return(remain[outpoint]);
+}
+
+.boxDetect<-function(depth,boxFactor)
+{
+  result=boxplot(depth,range=boxFactor,plot=F);
+  out=result$out[result$out<mean(depth)];
+  return(.findPos(out,depth));
+}
+
 TVDMSS=function(data,nCurve,nPoint)
 {
+  if(nCurve<3) stop("the number of curves must be greater than 2")
   pointwiseRank=t(apply(data,1,rank));
 
   shapeVar=matrix(0,nPoint,nCurve);
@@ -42,53 +87,9 @@ TVDMSS=function(data,nCurve,nPoint)
   return(list(TVD=TVD,MSS=MSS));
 }
 
-findPos=function(target,sc)
-{
-  outcome=NULL;
-  if(length(target)>0)
-  {
-    outcome=integer(length(target));
-    for(i in 1:length(outcome))
-      outcome[i]=which(sc==target[i]);
-  }
-  return(outcome);
-}
-
-fbplotDetect<-function(data,depth,shapeOutlier)
-{
-  nCurve=dim(data)[2];
-  nCentral=floor(nCurve/2);
-
-  remain=1:nCurve;
-  if(!is.null(shapeOutlier))
-  {
-    data=data[,-shapeOutlier]
-    depth=depth[-shapeOutlier]
-    remain=remain[-shapeOutlier];
-  }
-  index=order(depth,decreasing=T);
-
-  center=data[,index[1:nCentral]];
-  inf=apply(center,1,min);
-  sup=apply(center,1,max);
-  dist=1.5*(sup-inf);
-  upper=sup+dist;
-  lower=inf-dist;
-
-  outly=(data<=lower)+(data>=upper);
-  outpoint=which(colSums(outly)>0);
-  return(remain[outpoint]);
-}
-
-boxDetect<-function(depth,boxFactor)
-{
-  result=boxplot(depth,range=boxFactor,plot=F);
-  out=result$out[result$out<mean(depth)];
-  return(findPos(out,depth));
-}
-
 detectOutlier<-function(data,nCurve,nPoint,empFactor)
 {
+  if(nCurve<3) stop("the number of curves must be greater than 2")
   # compute TVD and MSS
   myDepth=TVDMSS(data,nCurve,nPoint);
   TVD=myDepth$TVD;
@@ -96,8 +97,8 @@ detectOutlier<-function(data,nCurve,nPoint,empFactor)
 
   # find outliers
   magOutlier=NULL;shapeOutlier=NULL;
-  shapeOutlier=boxDetect(MSS,empFactor)
-  magOutlier=fbplotDetect(data,TVD,shapeOutlier);
+  shapeOutlier=.boxDetect(MSS,empFactor)
+  magOutlier=.fbplotDetect(data,TVD,shapeOutlier);
 
   return(list(outlier=sort(c(shapeOutlier,magOutlier)),sOut=shapeOutlier,mOut=magOutlier,TVD=TVD,MSS=MSS));
 }
